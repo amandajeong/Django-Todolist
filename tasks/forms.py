8605057f74056edf677485b741d92c777dc6cf40
amanda jeong <sjeong8@villanova.edu>
2020-05-26 16:02:41 +0900

    Added due date functionality

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,10 +4,12 @@ from django.forms import ModelForm
 from .models import *
 
 
-class Taskform(forms.ModelForm):
-    title = forms.CharField(widget= forms.TextInput(attrs={
+class Taskform(ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={
         'placeholder': 'Add new task...'
     }))
+
+    due_date = forms.DateField(widget=forms.SelectDateWidget, required=False)
 
     class Meta:
         model = Task
--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date, timedelta
 
 
 # Create your models here.
@@ -6,6 +7,20 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField('due date', blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+    def in_the_past(self):
+        if self.due_date is not None:
+            return date.today() > self.due_date
+        else:
+            return False
+
+    def urgent(self):
+        if self.due_date is not None:
+            remaining = self.due_date - date.today()
+            return remaining == timedelta(days=1)
+        else:
+            return False
